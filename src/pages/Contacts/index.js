aa8e8b30eba3a3192f4f96681e83f0e0b5d275f7
Slava <kuzkin.slava888@gmail.com>
2021-07-19 18:01:57 +0300

    view_table_1

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,47 @@
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+import { useContacts } from './useContacts'
+import Typography from '@material-ui/core/Typography'
+import { ContactsTable } from './ContactsTable'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const useStyles = makeStyles((theme) => createStyles({
+    root: {
+        marginTop: theme.spacing(4)
+    },
+    headContainer: {
+        marginBottom: theme.spacing(3)
+    }
+}))
+
+
+
 export const Contacts = () => {
-    return <div>Contacts</div>
-}
+    const classes = useStyles();
+    const contacts = useContacts()
+
+
+    return (
+        <Container className={classes.root}>
+            <Grid container className={classes.headContainer}>
+                <Grid item xs={12}>
+                    <Typography variant="h3" component="h1">
+                        Contacts
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    {(() => {
+                        if (contacts.isLoading) {
+                            return <CircularProgress />
+                        }
+                        if (contacts.isError) {
+                            return <div>...error</div>
+                        }
+                        return <ContactsTable data={contacts.data} />
+                    })()}
+                </Grid>
+            </Grid>
+        </Container>
+    )
+} 
